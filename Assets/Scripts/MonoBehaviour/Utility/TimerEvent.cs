4e6cc147546a3d25507a6d6b1d040ce78ef827db
Alexander Velasco <alexandervelasco@live.com>
2015-03-12 16:51:38 +0800

    20150312

---
+++ Assets/Scripts/MonoBehaviour/Utility/TimerEvent.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class TimerEvent : EventTransceiverBehavior {
+
+	//serialized data
+	public bool repeat = false;
+	public float time = 0.0f;
+	public bool triggerOnStart = true;
+	public UnityEvent onTime = null;
+
+	private float currentTime = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		if (triggerOnStart)
+			currentTime = time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (currentTime > 0.0f)
+		{
+			currentTime -= Time.deltaTime;
+			if (currentTime <= 0.0f)
+			{
+				onTime.Invoke();
+				CallEvent(0, null, this);
+				if (repeat)
+					currentTime = time;
+			}
+		}
+	}
+
+	#region implemented abstract members of EventTransceiverBehavior
+
+	public override void ReceiveEvent (string eventName, object args, object sender)
+	{
+		if (currentTime <= 0.0f)
+			currentTime = time;
+	}
+
+	#endregion
+}
---
+++ Assets/Scripts/MonoBehaviour/World/RandomPositionPicker.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using PathologicalGames;
+
+public class RandomPositionPicker : EventTransceiverBehavior {
+
+	//serialized data
+	public string spawnPoolName = string.Empty;
+	public GameObject spawnedPrefab = null;
+	public int maximumSpawnAmount = 0;
+	public Vector3 spawnOffset = Vector3.zero;
+	public Vector3 maximumSpawnDistance = Vector3.zero;
+	public float minimumSpawnGapRadius = 0;
+
+	private SpawnPool spawnPool = null;
+
+	// Use this for initialization
+	public void Start () {
+		spawnPool = PoolManager.Pools[spawnPoolName];
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+	}
+
+	#region implemented abstract members of EventTransceiverBehavior
+
+	public override void ReceiveEvent (string eventName, object args, object sender)
+	{
+		StartCoroutine(SpawnInRandomPosition(spawnedPrefab));
+	}
+
+	#endregion
+
+	public void StartSpawn()
+	{
+		StartCoroutine(SpawnInRandomPosition(spawnedPrefab));
+	}
+
+	private IEnumerator SpawnInRandomPosition (GameObject spawnedPrefab)
+	{
+		ThreadSafeRandom r = new ThreadSafeRandom ();
+		Vector3 randomPosition = Vector3.zero;
+		bool hasNearbyInteractables = false;
+		do {
+			randomPosition = (new Vector3 (maximumSpawnDistance.x * (float)r.NextDouble (), maximumSpawnDistance.y * (float)r.NextDouble (), maximumSpawnDistance.z * (float)r.NextDouble ())) + spawnOffset;
+			IEnumerable<Transform> nearbyInteractables = spawnPool.Where(
+				t => Vector3.Distance(randomPosition, t.position) <= minimumSpawnGapRadius && spawnedPrefab.layer == t.gameObject.layer);
+			hasNearbyInteractables = nearbyInteractables.Count() > 0;
+			yield return null;
+		}
+		while (hasNearbyInteractables);
+		Transform spawn = spawnPool.Spawn(spawnedPrefab);
+		spawn.position = randomPosition;
+	}
+}
