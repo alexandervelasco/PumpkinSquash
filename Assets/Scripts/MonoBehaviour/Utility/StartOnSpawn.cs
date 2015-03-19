ede31b8bf397709dbc31e4da4abc9f14a51c3c86
Alexander Velasco <alexandervelasco@live.com>
2015-03-19 16:58:27 +0800

    20150319

---
+++ Assets/Scripts/MonoBehaviour/Utility/StartOnSpawn.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartOnSpawn : MonoBehaviour {
+
+	//serialized data
+	public bool broadcast = true;
+
+	private bool initialStartCalled = false;
+
+	// Use this for initialization
+	void Start () {
+		initialStartCalled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnSpawned() {
+		if (initialStartCalled && broadcast)
+			gameObject.BroadcastMessage("Start");
+		else if (initialStartCalled && !broadcast)
+			gameObject.SendMessage("Start");
+	}
+}
