63494f090e1a50039b57933efe5d94607720ff05
Alexander Velasco <alexandervelasco@live.com>
2015-03-27 16:49:57 +0800

    20150327

---
+++ Assets/Scripts/MonoBehaviour/Utility/OnStartEvent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.Events;
+
+public class OnStartEvent : MonoBehaviour {
+
+	//serialized data
+	public UnityEvent onStart;
+
+	// Use this for initialization
+	void Start () {
+		onStart.Invoke();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
